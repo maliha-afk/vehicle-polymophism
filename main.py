d8fd70d7249635f7c355b72c6924a142946f0fc3
maliha-afk <24amaliha@gmail.com>
2024-10-24 17:32:20 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,75 @@
+class TATA:
+    def max_speed():
+        print("TATA's max speed is 180Kmph")
+
+    def fuel_type():
+        print("TATA's fuel type is (well engine because it's specific) 1 Diesel Engine, 1 Petrol Engine and 1 CNG Engine")
+    
+
+
+
+class TOYATO:
+    def max_speed():
+        print("TOYATO's max speed is 255kph (158 mph)")
+
+    def fuel_type():
+        print("TOYATO's fuel type is unleaded gasoline")
+
+
+
+
+class MERCEDES:
+    def max_speed():
+        print("MERCEDES's max speed is 202 mph")
+
+    def fuel_type():
+        print("MERCEDES's fuel type is  premium")
+
+
+
+class BMW:
+    def max_speed():
+        print("BMW's max speed is 191 mph")
+
+    def fuel_type():
+        print("BMW's fuel type is premium gasoline")
+
+
+class  Koenigsegg_Jesko_Absolut :
+    def max_speed():
+        print(" Koenigsegg Jesko Absolut 's max speed is 330 MPH")
+
+    def fuel_type():
+        print(" Koenigsegg Jesko Absolut 's fuel type is E85 fuel")
+
+
+
+tata=TATA.max_speed()
+tata2=TATA.fuel_type()
+print(f"{tata}{tata2}")
+
+print("-----------------------")
+
+tata=TOYATO.max_speed()
+tata2=TOYATO.fuel_type()
+print(f"{tata}{tata2}")
+
+print("-----------------------")
+
+tata=MERCEDES.max_speed()
+tata2=MERCEDES.fuel_type()
+print(f"{tata}{tata2}")
+
+print("-----------------------")
+
+tata=BMW.max_speed()
+tata2=BMW.fuel_type()
+print(f"{tata}{tata2}")
+
+print("-----------------------")
+
+tata=Koenigsegg_Jesko_Absolut.max_speed()
+tata2=Koenigsegg_Jesko_Absolut.fuel_type()
+print(f"{tata}{tata2}")
+
+print("-----------------------")
